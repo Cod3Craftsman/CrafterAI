3eb50e7855e86df3f5778ad2f0695f025081ff27
Ankit Gupta <ag5756042@gmail.com>
2026-07-09 01:22:00 +0530

    Initial commit

---
+++ backend/services/auth/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+dotenv.config()
+
+const port = process.env.PORT
+const app = express()
+
+app.get("/" , (req , res) => {
+  res.json({message : "Hello from auth!"})
+})
+
+app.listen(port , () => {
+  console.log(`Auth Started at port: ${port}`)
+  connectDb()
+})
