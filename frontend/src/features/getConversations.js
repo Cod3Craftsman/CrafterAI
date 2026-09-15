a38de58a9442f93ba4b045584337d101436c9cae
Ankit Gupta <ag5756042@gmail.com>
2026-09-15 17:26:58 +0530

    feat: remove console log from getConversations function

--- frontend/src/features/getConversations.js
+++ frontend/src/features/getConversations.js
@@ -3,7 +3,6 @@ import api from "../../utils/axios.js";
 export const getConversations = async () => {
   try {
     const {data} = await api.get("/api/chat/get-conversations")
-    console.log(data)
     return data
   } catch (error) {
     console.log(error)
